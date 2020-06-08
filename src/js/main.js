75d2168c99c143a5a52cf23977a55bff6a932463
Rasta-jaZz <rasta.jazz@yandex.ru>
2020-06-09 00:34:38 +0400

    add fetch input value

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,7 @@
 import "./slider"
 import { modals } from "./modules/modals"
 import { tabs } from "./modules/tabs"
+import { forms } from "./modules/forms"
 
 window.addEventListener("DOMContentLoaded", () => {
 	modals(
@@ -8,13 +9,17 @@ window.addEventListener("DOMContentLoaded", () => {
 		".popup_engineer",
 		".popup_engineer .popup_close"
 	)
+
 	modals(".phone_link", ".popup", ".popup .popup_close")
 
 	tabs(".glazing_slider", ".glazing_block", ".glazing_content", "active")
+
 	tabs(
 		".decoration_slider",
 		".no_click",
 		".decoration_content >div >div",
 		"after_click"
 	)
+
+	forms()
 })
---
+++ src/js/modules/forms.js
@@ -0,0 +1,59 @@
+export function forms() {
+	const form = document.querySelectorAll("form"),
+		inputs = document.querySelectorAll("input"),
+		phoneInputs = document.querySelectorAll('input[name="user_phone"]')
+
+	const message = {
+		loading: "Загрузка...",
+		success: "Спасибо! Скоро мы с вами свяжемся",
+		fail: "Ошибка сервера...",
+	}
+
+	const postData = async (url, data) => {
+		document.querySelectorAll(".status").textContent = message.loading
+
+		let res = await fetch(url, {
+			method: "POST",
+			body: data,
+		})
+
+		return await res.text()
+	}
+
+	const clearForm = () => {
+		inputs.forEach((elem) => (elem.value = ""))
+	}
+
+	phoneInputs.forEach((elem) => {
+		elem.addEventListener("input", () => {
+			elem.value = isNaN(+elem.value) ? "" : +elem.value
+		})
+	})
+
+	form.forEach((elem) => {
+		elem.addEventListener("submit", (e) => {
+			e.preventDefault()
+
+			const statusMessage = document.createElement("div")
+			statusMessage.classList.add("status")
+			elem.appendChild(statusMessage)
+
+			const formData = new FormData(elem)
+
+			postData("assets/server.php", formData)
+				.then((res) => {
+					console.log("res :>> ", res)
+					statusMessage.textContent = message.success
+				})
+				.catch(() => {
+					statusMessage.textContent = message.fail
+				})
+				.finally(() => {
+					clearForm()
+					setTimeout(() => {
+						statusMessage.remove()
+					}, 5000)
+				})
+		})
+	})
+}
